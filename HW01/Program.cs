6a20c50cdaea0b28182e949eddd71c7e7773d59f
Radel <latyp47@mail.ru>
2022-04-30 17:44:46 +0300

    Первое задание

---
+++ HW01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите 2 числа для сравнения:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1>number2)
+{
+    
+    Console.WriteLine ("Вывод: Первое число больше второго числа");
+}
+if (number1<number2)
+{
+    
+    Console.WriteLine ("Вывод: Второе число больше первого числа");
+}
