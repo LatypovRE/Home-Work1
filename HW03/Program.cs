ec00a2860b99b1772c1ca459bb20a5dc261fb08d
Radel <latyp47@mail.ru>
2022-04-30 21:23:49 +0300

    Задание 3

---
+++ HW03/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите 3 числа для сравнения:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if ((number1>number2) & (number1>number3))
+{
+    
+    Console.WriteLine ("Вывод: Первое число наибольшее");
+}
+if ((number2>number1) & (number2>number3))
+{
+    
+    Console.WriteLine ("Вывод: Второе число наибольшее");
+}
+if ((number3>number1) & (number3>number2))
+{
+    
+    Console.WriteLine ("Вывод: Третье число наибольшее");
+}
